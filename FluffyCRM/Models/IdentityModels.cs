f57d723c57d78d46f2982891c0669a09582569bc
logicpretzel <dunham5@mail.nmc.edu>
2017-02-13 09:25:11 -0500

    minor 1

--- FluffyCRM/Models/IdentityModels.cs
+++ FluffyCRM/Models/IdentityModels.cs
@@ -3,12 +3,38 @@ using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using System.ComponentModel.DataAnnotations;
 
 namespace FluffyCRM.Models
 {
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+
+        [Display(Name = "Last Name")]
+        [MaxLength(50)]
+        public string LastName { get; set; }
+
+
+        [Display(Name = "First Name")]
+        [MaxLength(50)]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Address")]
+        public string Address { get; set; }
+
+        [Display(Name = "City")]
+        [MaxLength(50)]
+        public string City { get; set; }
+
+        [Display(Name = "State")]
+        [MaxLength(50)]
+        public string State { get; set; }
+
+        [Display(Name = "Zip")]
+        [MaxLength(10)]
+        public string Zip { get; set; }
+
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
